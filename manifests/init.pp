6c9fed01fd6e27075a5058d99c0d35ab6beebc1a
Joshua Bussdieker <jbussdieker@gmail.com>
2013-07-22 12:43:03 -0700

    Support more options

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-class memcached {
+class memcached(
+  $enable_memcached = 'yes',
+  $log_file         = '/var/log/memcached.log',
+  $memory_max       = 64,
+  $listen_port      = 11211,
+  $listen_ip        = '127.0.0.1',
+  $memcache_user    = 'memcache',
+  $connection_limit = 1024
+) {
 
   class {'memcached::package':
     notify => Class['memcached::service'],
